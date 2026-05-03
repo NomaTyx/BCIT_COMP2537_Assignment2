b25fc4270d1f9a11d7c67c2cb1dbdc54373325d6
Julia Ziebart <jjziebart99@gmail.com>
2026-05-03 01:08:56 -0700

    init DB file

--- db.js
+++ db.js
@@ -1,1 +1,11 @@
-mongodb+srv://nomatyx:3rXjr9LZfJT6nPgr@bcit.bcxaqyh.mongodb.net/?appName=BCIT
+require('dotenv').config();
+
+const mongodb_host = process.env.MONGODB_HOST;
+const mongodb_user = process.env.MONGODB_USER;
+const mongodb_password = process.env.MONGODB_PASSWORD;
+
+const MongoClient = require("mongodb").MongoClient;
+const atlasURI = `mongodb+srv://nomatyx:3rXjr9LZfJT6nPgr@bcit.bcxaqyh.mongodb.net/?appName=BCIT`;
+var database = new MongoClient(atlasURI, {});
+module.exports = {database};
+
